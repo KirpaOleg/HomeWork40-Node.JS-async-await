6e8a965240f3c73e50b20faf57454e69fa524b52
KirpaOleg <profix.ok@gmail.com>
2021-11-28 22:15:15 +0200

    level2

--- routes/person.js
+++ routes/person.js
@@ -9,17 +9,38 @@ const getPerson = async(getURL, res) => {
   const resultPerson = await axios.get(getURL);
   const name = resultPerson.data.name;
   const films = resultPerson.data.films;
+
   let listFilms = '';
   films.forEach(element => {
     listFilms += `${element}<br>\n`;
   });
-  res.send(`<table><tr><td>${name}<br></td></tr><tr><td>${listFilms}</td></tr></table>`)
+
+  let listFilmTitle = [];
+  for await (let param of films) {
+    const result = await axios.get(param);
+    listTitle = result.data.title;
+    listFilmTitle.push(`${listTitle}<br>`);
+  }
+
+  res.send(`<table>
+              <tr>
+                <td>${name}<br></td>
+              </tr>
+              <tr>
+                <td>${listFilms}<br></td>
+              </tr>
+              <tr>
+                <td>${listFilmTitle}<br></td>
+              </tr>
+            </table>`)
 }
+
+  
+
 
 router.get('/:param1', (req, res) => {
   const getURL = `${URL}${req.params.param1}`;
-  console.log('>>>>>>', getURL);
- 
+  
   getPerson(getURL, res)
    
 });
